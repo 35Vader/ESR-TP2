0b2d8e55726fb1ab3bbccb067cccda7a8c75a594
AdrianoFeixa <103319502+AdrianoFeixa@users.noreply.github.com>
2023-11-16 16:30:11 +0000

    Update Node.java
    sccc

--- Node.java
+++ Node.java
@@ -1,27 +1,39 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.SocketException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Node {
     private String ip;
-    private int porta;
-    private Set<String> vizinhos = new HashSet<>();
-    private String ip_bootstraper;
-    private ConnectionManagerIdentifier connectionManagerIdentifier;
 
-    private InetAddress group;
-    private MulticastSocket multicastSocket;
+    ///private ReentrantLock vizinhos_lock;
+
+    private int porta_ouve;
+    private int porta_escreve;
+
+    private Set<String> vizinhos = new HashSet<>();
+    private int porta_bootstraper;
+
+    private Socket escreve;
+    private Socket ouve;
+
+    //private InetAddress group;
+    //private MulticastSocket multicastSocket;
 
     // Construtor
-    public Node(String ip, int porta, ConnectionManagerIdentifier connectionManagerIdentifier, String ip_bootstraper, String multicastGroup, int multicastPort) throws IOException {
+    public Node(String ip, int porta_ouve, int porta_escreve , int porta_bootstraper) throws IOException {
+
         this.ip = ip;
-        this.porta = porta;
-        this.ip_bootstraper = ip_bootstraper;
-        this.connectionManagerIdentifier = connectionManagerIdentifier;
+        this.porta_ouve    = porta_ouve;
+        this.porta_escreve = porta_escreve;
+        this.porta_bootstraper = porta_bootstraper;
+        this.escreve = new Socket(ip,porta_escreve);
+        this.ouve = new Socket(ip,porta_ouve);
 
         // Configuração do socket multicast
         ///this.group = InetAddress.getByName(multicastGroup);
@@ -29,70 +41,36 @@ public class Node {
         //this.multicastSocket.joinGroup(group);
 
         // Inicialização do Connection Manager
-        connectionManagerIdentifier.setConnectionManagers(this.ip, this.porta);
     }
 
-    private synchronized boolean isVizinho(String ip) {
-        return vizinhos.contains(ip);
+    public void setVizinhos() throws IOException{
+        new Thread(() -> {
+            Socket bootstraper = null;
+            BufferedReader leitor = null;
+            PrintWriter escritor = null;
+            String vizinhos;
+            try {
+
+                bootstraper = new Socket(ip, porta_bootstraper);
+                leitor = new BufferedReader(new InputStreamReader(bootstraper.getInputStream()));
+                escritor = new PrintWriter(bootstraper.getOutputStream(), true);
+
+                escritor.println("Vizinhos");
+
+                //while ( leitor.readLine() == null) wait();
+
+                vizinhos = leitor.readLine();
+
+                String[] ips = vizinhos.split(",");
+                for (String ip : ips) {
+                    this.vizinhos.add(ip);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
     }
 
-    public void ler() throws IOException {
-        byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-        // Recebe dados do multicast
-        multicastSocket.receive(packet);
-        String mensagem = new String(packet.getData(), 0, packet.getLength());
-
-        System.out.println("Mensagem recebida: " + mensagem);
-    }
-     
-    private void processarMensagem(String mensagem) {
-        // Suponha que a mensagem seja um evento específico, como "INICIO_STREAM"
-        switch (mensagem) {
-            case "INICIO_STREAM":
-                // Lógica para lidar com o início de uma nova transmissão
-                System.out.println("Iniciando nova transmissão...");
-                break;
-            case "FIM_STREAM":
-                // Lógica para lidar com o término de uma transmissão
-                System.out.println("Transmissão concluída.");
-                break;
-            // Adicione outros casos conforme necessário
-            default:
-                // Lógica padrão para mensagens desconhecidas
-                System.out.println("Mensagem desconhecida: " + mensagem);
-                break;
-        }
-    }
-
-    public void escrever(String mensagem) throws IOException {
-        // Envia dados para o grupo multicast
-        multicastSocket.send(new DatagramPacket(mensagem.getBytes(), mensagem.length(), group, porta));
-    }
-
-    public void lerBootstraper() throws IOException {
-        connectionManagerIdentifier.getConnectionManager(ip_bootstraper).read();
-    }
-
-    public void escreverBootstraper() throws IOException {
-        connectionManagerIdentifier.getConnectionManager(ip_bootstraper).send("vizinhos", "vizinhos");
-    }
-
-    public String receberBootstraper() throws IOException, InterruptedException {
-        String s = connectionManagerIdentifier.getConnectionManager(ip_bootstraper).receive("vizinhos");
-        String[] ips = s.split(",");
-        for (String ip : ips) {
-            vizinhos.add(ip);
-        }
-        return s;
-    }
-
-    public synchronized void adicionarVizinho(String ip) {
-        vizinhos.add(ip);
-    }
-    
-    public synchronized void removerVizinho(String ip) {
-        vizinhos.remove(ip);
-    }
 }
