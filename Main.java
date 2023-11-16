f7c7de836f8c784765c102f75d46e66480cc6754
Alexandra Candeias <48985572+35Vader@users.noreply.github.com>
2023-11-16 14:20:55 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
-
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
             ConnectionManagerIdentifier connectionManagerIdentifier = new ConnectionManagerIdentifier();
+            connectionManagerIdentifier.setConnectionManagers("192.150.20", 1234);
+            connectionManagerIdentifier.setConnectionManagers("192.150.21", 1235);
+            connectionManagerIdentifier.setConnectionManagers("192.150.22", 1236);
 
+            connectionManagerIdentifier.setConnectionManagers("192.150.30", 4321);
+            // Bootstraper bootstraper = 
+            //Node node1 = new Node("192.150.20", 1234, connectionManagerIdentifier, "192.150.30", "230.0.0.1", 9876);
     }
 }
--- Node.java
+++ Node.java
@@ -1,68 +1,98 @@
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Node {
-
     private String ip;
     private int porta;
-    private Set<String> vizinhos = new HashSet<>(); // ip
+    private Set<String> vizinhos = new HashSet<>();
     private String ip_bootstraper;
     private ConnectionManagerIdentifier connectionManagerIdentifier;
 
-    public Node(String ip, Integer porta, ConnectionManagerIdentifier connectionManagerIdentifier, String ip_bootstraper) throws IOException {
+    private InetAddress group;
+    private MulticastSocket multicastSocket;
+
+    // Construtor
+    public Node(String ip, int porta, ConnectionManagerIdentifier connectionManagerIdentifier, String ip_bootstraper, String multicastGroup, int multicastPort) throws IOException {
         this.ip = ip;
         this.porta = porta;
         this.ip_bootstraper = ip_bootstraper;
         this.connectionManagerIdentifier = connectionManagerIdentifier;
-        connectionManagerIdentifier.setConnectionManagers(this.ip,this.porta);
+
+        // Configuração do socket multicast
+        this.group = InetAddress.getByName(multicastGroup);
+        this.multicastSocket = new MulticastSocket(multicastPort);
+        this.multicastSocket.joinGroup(group);
+
+        // Inicialização do Connection Manager
+        connectionManagerIdentifier.setConnectionManagers(this.ip, this.porta);
     }
-    private boolean is_vizinho(String ip){
-        boolean a = false;
-        for (String ips: this.vizinhos) {
-            if (ips.equals(ip)) {a = true; break;}
+
+    private synchronized boolean isVizinho(String ip) {
+        return vizinhos.contains(ip);
+    }
+
+    public void ler() throws IOException {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+        // Recebe dados do multicast
+        multicastSocket.receive(packet);
+        String mensagem = new String(packet.getData(), 0, packet.getLength());
+
+        System.out.println("Mensagem recebida: " + mensagem);
+    }
+     
+    private void processarMensagem(String mensagem) {
+        // Suponha que a mensagem seja um evento específico, como "INICIO_STREAM"
+        switch (mensagem) {
+            case "INICIO_STREAM":
+                // Lógica para lidar com o início de uma nova transmissão
+                System.out.println("Iniciando nova transmissão...");
+                break;
+            case "FIM_STREAM":
+                // Lógica para lidar com o término de uma transmissão
+                System.out.println("Transmissão concluída.");
+                break;
+            // Adicione outros casos conforme necessário
+            default:
+                // Lógica padrão para mensagens desconhecidas
+                System.out.println("Mensagem desconhecida: " + mensagem);
+                break;
         }
-        return a;
-    }
-    public void ler(String ip) throws IOException {
-
-        if(is_vizinho(ip) == true) connectionManagerIdentifier.getConnectionManager(ip).read();
-
-        else System.out.println("Não é vizinho!!!");
     }
 
-    public void ler_bootstraper() throws IOException {
-        connectionManagerIdentifier.getConnectionManager(this.ip_bootstraper).read();
+    public void escrever(String mensagem) throws IOException {
+        // Envia dados para o grupo multicast
+        multicastSocket.send(new DatagramPacket(mensagem.getBytes(), mensagem.length(), group, porta));
     }
 
-    public void escrever(String ip, String tipo, String mensagem) throws IOException {
-        if(is_vizinho(ip) == true) connectionManagerIdentifier.getConnectionManager(ip).send(tipo, mensagem);
-
-        else System.out.println("Não é vizinho!!!");
+    public void lerBootstraper() throws IOException {
+        connectionManagerIdentifier.getConnectionManager(ip_bootstraper).read();
     }
 
-    public void escrever_bootstraper() throws IOException {
-        connectionManagerIdentifier.getConnectionManager(this.ip_bootstraper).send("vizinhos", "vizinhos");
+    public void escreverBootstraper() throws IOException {
+        connectionManagerIdentifier.getConnectionManager(ip_bootstraper).send("vizinhos", "vizinhos");
     }
 
-    public String receber(String ip, String tipo) throws InterruptedException {
-        String s;
-        if(is_vizinho(ip) == true) s = connectionManagerIdentifier.getConnectionManager(ip).receive(tipo);
-
-        else s =  "Não é vizinho!!!";
-
+    public String receberBootstraper() throws IOException, InterruptedException {
+        String s = connectionManagerIdentifier.getConnectionManager(ip_bootstraper).receive("vizinhos");
+        String[] ips = s.split(",");
+        for (String ip : ips) {
+            vizinhos.add(ip);
+        }
         return s;
     }
 
-    // ip porta, ip porta,...
-    // vizinhos = "121.191.51.101,121.191.51.101,121.191.51.101"
-    public void receber_bootstraper() throws InterruptedException, IOException {
-
-        String s = connectionManagerIdentifier.getConnectionManager(this.ip_bootstraper).receive("vizinhos");
-        // [ip, ip, ...]
-        String[] ips = s.split(",");
-
-        for (String ip : ips) vizinhos.add(ip);
+    public synchronized void adicionarVizinho(String ip) {
+        vizinhos.add(ip);
+    }
+    
+    public synchronized void removerVizinho(String ip) {
+        vizinhos.remove(ip);
     }
 }
-
