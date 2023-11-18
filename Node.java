ffe67b27210e17281625d3a71e32ad0fdf03bb1b
AdrianoFeixa <103319502+AdrianoFeixa@users.noreply.github.com>
2023-11-18 19:15:18 +0000

    Update Node.java

--- Node.java
+++ Node.java
@@ -3,95 +3,156 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 
+//mensagens para nós = ip-type|meng
 
 public class Node {
-    private String ip;
+    private  final String ip;
 
     ///private ReentrantLock vizinhos_lock;
+    private final ReentrantLock l_fila_de_espera = new ReentrantLock();
 
-    private int porta_ouve;
-    private int porta_escreve;
+    // lock da lista de vizinhos
+    private final ReentrantLock l_vizinhos = new ReentrantLock();
 
-    private HashMap<String,Integer> vizinhos = new HashMap<>();
-    private int porta_bootstraper;
-    private ArrayList<String> fila_de_espera;
+    private final int porta;
+    // ip->porta
+    private final HashMap<String,Integer> vizinhos = new HashMap<>();
+    private final int porta_bootstraper;
+    private final HashMap < String, ArrayList<String> > fila_de_espera = new HashMap<>(); // ip -> [men]
 
     // Construtor
-    public Node(String ip, int porta_ouve, int porta_escreve , int porta_bootstraper) throws IOException {
+    public Node(String ip, int porta, int porta_bootstraper) throws IOException {
 
         this.ip = ip;
-        this.porta_ouve    = porta_ouve;
-        this.porta_escreve = porta_escreve;
+        this.porta = porta;
         this.porta_bootstraper = porta_bootstraper;
     }
-    //recessor geral
-    public void ressecionista(){
-        // Inicie o servidor do Node
-        new Thread(() -> {
-                try (ServerSocket ouvinte_mestre = new ServerSocket(this.porta_ouve)) {
-                    System.out.println("Servidor iniciado na porta " + this.porta_ouve);
 
-                    // Thread para leitura de mensagens do console (entrada)
+    private void SmartPut(String ip, String mensagem, HashMap<String,ArrayList<String>> fila){
+        ArrayList<String> temp;
+        if((temp = fila.get(ip)) != null) temp.add(mensagem);
+
+        else{
+            temp = new ArrayList<String>();
+            temp.add(mensagem);
+            fila.put(ip,temp);
+        }
+
+    }
+    private String ChooseKey(HashMap<String,ArrayList<String>> fila){
+        Set<String> v = new HashSet<>(); // vizinhos que mandaram mensagues
+
+        for (String key: fila.keySet() ) {
+                if(!fila.get(key).isEmpty()) v.add(key);
+        }
+
+        // Convert the Set to a List
+        List<String> myList = new ArrayList<>(v);
+
+        // Use Random to generate a random index
+        Random random = new Random();
+        int randomIndex = random.nextInt(myList.size());
+
+        return myList.get(randomIndex);
+
+    }
+
+    private void SetVizinhos(String vizinhos) {
+        // [121.191.51.101:12341,121.191.52.101:12342]
+        String[] ips_portas = vizinhos.split(",");
+
+        for (String ip_porta : ips_portas) {
+            // [121.191.51.101, 12341]
+            String[] vizinho = ip_porta.split(":");
+
+            this.vizinhos.put(vizinho[0], Integer.parseInt(vizinho[1]));
+        }
+    }
+
+    //recessor geral
+    public void servidor(){
+        // Uma especie de recessionista
+        new Thread(() -> {
+                try (ServerSocket ouvinte_mestre = new ServerSocket(this.porta)) {
+                    // ligação entre um vizinho e 'eu' (eu sou um Node)
+
+                    // Thread para leitura de mensagens de todos os seus vizinhos
                     new Thread(() -> {
                         try {
                             Socket ouvinte = ouvinte_mestre.accept();
                             BufferedReader leitor_vizinho = new BufferedReader(new InputStreamReader(ouvinte.getInputStream()));
                             String mensagem;
                             while (true) {
-                                if ( (mensagem = leitor_vizinho.readLine()) != null) this.fila_de_espera.add(mensagem);
+                                //ip-tipo|mensg
+                                if ( (mensagem = leitor_vizinho.readLine()) != null){
+                                    // [ip,tipo|mensg]
+                                    String [] ip_mensg = mensagem.split("-");
+                                    try {
+                                        l_fila_de_espera.lock();
+                                        SmartPut(ip_mensg[0], ip_mensg[1],this.fila_de_espera);
+                                    }
+                                    finally {
+                                        l_fila_de_espera.unlock();}
+                                    }
                                 }
-                            } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                            }
+                        catch (IOException e) {e.printStackTrace();}
+
+
                     }).start();
-                } catch (IOException e) {
-                    e.printStackTrace(); }});
+
+                    // uma especie de capataz
+                    new Thread(() -> {
+                        try {
+                            Socket escritor = ouvinte_mestre.accept();
+                            PrintWriter escritor_vizinho = new PrintWriter(escritor.getOutputStream());
+                            while (true) {
+                                if (!this.fila_de_espera.values().isEmpty()) {
+                                    String mensagem;
+                                    try {
+                                        l_fila_de_espera.lock();
+                                        String key = ChooseKey(this.fila_de_espera);
+                                        mensagem = this.fila_de_espera.get(key).get(0);
+                                        this.fila_de_espera.get(key).remove(0);
+                                    } finally {
+                                        l_fila_de_espera.unlock();}
+
+                                    // [tipo, meng]
+                                    String [] mensagem_split = mensagem.split("|");
+
+                                    switch (mensagem_split[0]) {
+                                        case "ok?":
+                                            escritor_vizinho.println("ok");
+                                            break;
+
+                                        case "Vizinhos":
+                                            // "121.191.51.101:12341,121.191.52.101:12342"
+                                            try {
+                                               l_vizinhos.lock();
+                                                SetVizinhos(mensagem_split[1]);
+                                            } finally{l_vizinhos.unlock();}
+
+                                        case "stream":
+                                            break;
+
+                                        default:
+                                            System.out.println("Mensagem inválida");
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {e.printStackTrace();}
+                    }).start();
+
+                } catch (IOException e) { e.printStackTrace(); }});
     }
 
 
 
     // primeira fase defenir os vizinhos
-    public void setVizinhos() throws IOException{
-        new Thread(() -> {
-            Socket bootstraper = null;
-            BufferedReader leitor = null;
-
-            try {
-                bootstraper = new Socket(ip, porta_bootstraper);
-                leitor = new BufferedReader(new InputStreamReader(bootstraper.getInputStream()));
-
-                while (leitor.readLine() == null);
-
-                // 121.191.51.101:12341,121.191.52.101:12342
-                String vizinhos = leitor.readLine();
-
-                // [121.191.51.101:12341,121.191.52.101:12342]
-                String[] ips_portas = vizinhos.split(",");
-
-                for (String ip_porta : ips_portas) {
-                    // [121.191.51.101, 12341]
-                    String[] vizinho = ip_porta.split(":");
-
-                    this.vizinhos.put (vizinho[0], Integer.parseInt(vizinho[1]) );
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (leitor != null) leitor.close();
-                    if (bootstraper != null) bootstraper.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+    public void setVizinhos2() throws IOException{
 
         new Thread(() -> {
             Socket bootstraper = null;
@@ -101,7 +162,7 @@ public class Node {
                 bootstraper = new Socket(ip, porta_bootstraper);
                 escritor = new PrintWriter(bootstraper.getOutputStream(), true);
 
-                escritor.println("Vizinhos," + this.ip);
+                escritor.println(this.ip+ "-" + "Vizinhos|");
 
             } catch (IOException e) {
                 e.printStackTrace();
